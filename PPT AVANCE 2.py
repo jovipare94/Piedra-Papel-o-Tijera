6ac063eea6d5e52ac848df7c3e1bcabf4aa0ccb6
jovipare94 <151700269+jovipare94@users.noreply.github.com>
2023-11-24 18:46:45 -0500

    Avance 2 PPT

---
+++ PPT AVANCE 2.py
@@ -0,0 +1,7 @@
+print("Hola, bienvenido al juego Piedra, Papel o Tijera")
+print("A continuación, te invito que sigas los paso que te daré a escoger. ¡Que lo disfrutes!")
+import random
+op= ["piedra","papel","tijera"]
+
+while True>
+user= input("Elije> piedra, papel o tijera")
